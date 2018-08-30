27705c442eac8e3f0ed3969172b7bf94d8204730
betho <hgomescostajr@gmail.com>
2018-08-30 17:56:59 -0300

    las commit for destory

--- js/notification.js
+++ js/notification.js
@@ -1,11 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
- 
-    //Se não tiver suporte a Notification manda um alert para o usuário
-                                Notification.requestPermission();
-                                new Notification('Chat Geocorr', {
-                                    icon: 'http://geocorrambiental.com.br/webfiles/images/logo.png',
-                                    body: "Novo cliente abriu o chat",
-                                });
-       });
-   
-   
+﻿class PushNotification{
+    static sendRequest(title , icon , textBody){
+        Notification.requestPermission();
+        new Notification(title, {
+            icon: HOST+icon,
+            body: textBody,
+        });
+    } 
+ }
