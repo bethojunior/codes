1bfd51c37bbf733c2931b351d0db78e9473e509f
jr <hgomescostajr@gmail.com>
2018-11-22 11:37:28 -0200

    ok

--- PadraoProjeto/p2/mvc/controllers/HomeController.php
+++ PadraoProjeto/p2/mvc/controllers/HomeController.php
@@ -3,9 +3,9 @@
 
         function actionIndex()
         { 
-            $js  = ['utils/navBar','utils/navbarHomeMobile','modulos/home'];
-            $css = ['assets','reset','home','navbarTransparent','menuMobile','menuMobileHome','footer','preload'];
-            $views = ['preload/index','nav/navBarTransparent','nav/navBarMobileHome','home/index','import/footer'];
+            $js  = [];
+            $css = ['assets','preload'];
+            $views = ['preload/index','home/index'];
 
             $seo = new stdClass();
             $seo->description   = 'Aplicativo de Taxi de Fortaleza que conecta hotelaria à taxistas, oferecendo o melhor preço em corridas com destinos à rodoviária/aeroporto. Acesse e descubra.';
