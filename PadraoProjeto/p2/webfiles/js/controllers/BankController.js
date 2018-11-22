0c70e365a8c05585418c51f58c84c356fd4bc606
jr <hgomescostajr@gmail.com>
2018-11-22 11:23:22 -0200

    apagando inuteis

--- PadraoProjeto/p2/webfiles/js/controllers/BankController.js
+++
@@ -1,9 +0,0 @@
-class BankController{
-    async getAll(callback){
-        await apis.bankGetAll().then(res => {
-            callback(res.data)
-        }).catch(err => {
-            console.log(err)
-        }) 
-    }
-}
--- PadraoProjeto/p2/webfiles/js/controllers/EmployeeController.js
+++
@@ -1,8 +0,0 @@
-class EmployeeController{
-
-    register(employee){
-        return new Promise((resolve)=>{
-            ConnectionServer.sendRequest("Customer/RegisterPartner","POST",employee,resolve)
-        });
-    }
-}
