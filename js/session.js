d7f543e76bdd8d95b691e4efdae9081c98f648a4
jr <hgomescostajr@gmail.com>
2018-11-27 17:55:50 -0200

    classe de tratar cookies via js

--- js/session.js
+++ js/session.js
@@ -33,3 +33,37 @@ class Session {
         Session.set(nameStorage, data);
     }
 }
+
+
+class SessionCookie{
+    static getCookie(name) {
+        $.cookie.json = true;
+        return $.cookie(name);
+    }
+
+
+    static setCookie(name, value) {
+        $.cookie.json = true;
+        $.cookie(name,value, { path: '/' });
+    }
+
+    static deleteCookie(name) {
+        $.removeCookie('user', { path: '/' });
+    }
+
+
+    static getValueInSession(id,attribute){
+        const session = Session.getCookie(id);
+
+        if(!session)
+            return false;
+
+        if(Array.isArray(session))
+            return session.map(item => {
+                return item[attribute];
+            });
+
+        return session[attribute];
+
+    }
+}
